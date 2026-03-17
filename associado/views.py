f157b3abf3da748ea81cddc9235757e50f8b4389
Nayan Pereira <nayan.estudos@gmail.com>
2026-03-17 12:31:09 -0300

    integração de dados mock na tabela de associados e ajustes de CSS

--- associado/views.py
+++ associado/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'associado/index.html')
+    # criar dados mock do associado: ID, nome, status, (ativo/inativo)
+    # criar uma lista de dicionários com dados mock para representar os usuários
+    associado = [
+    {'id':'001','nome':'Romulo Jackson','status':'Ativo'},
+    {'id':'002','nome':'David Quilan','status':'Ativo'},
+    {'id':'003','nome':'Nayan Jonhson','status':'Inativo'},
+    {'id':'004','nome':'Cassio Dylan','status':'Ativo'},
+    {'id':'005','nome':'Welligton Cliton','status':'Inativo'},
+    ]
+    # O django buscara automaticamente dentro da pasta templates
+    # 2. Prepara o dicionário de contexto. 
+    # A chave 'lista_associado' é exatamente o nome que você usou no {% for %} do HTML.
+    contexto = {
+        'lista_associado': associado
+    }
+    # 3. Passa o contexto como terceiro argumento no render
+    return render(request, 'associado/index.html', contexto)
 
 def perfil(request):
     return render(request, 'associado/perfil.html')
