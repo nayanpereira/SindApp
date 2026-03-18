efb82ffaae55c8a37ddc385f29160c7535c6f679
Nayan Pereira da Silva <nayan59184236@edu.df.senac.br>
2026-03-18 20:56:44 -0300

    adicionado associados manualmente

--- associado/views.py
+++ associado/views.py
@@ -3,12 +3,20 @@ from django.shortcuts import render
 def index(request):
     # criar dados mock do associado: ID, nome, status, (ativo/inativo)
     # criar uma lista de dicionários com dados mock para representar os usuários
+    
     associado = [
-    {'id':'001','nome':'Romulo Jackson','status':'Ativo'},
-    {'id':'002','nome':'David Quilan','status':'Ativo'},
-    {'id':'003','nome':'Nayan Jonhson','status':'Inativo'},
-    {'id':'004','nome':'Cassio Dylan','status':'Ativo'},
-    {'id':'005','nome':'Welligton Cliton','status':'Inativo'},
+        {'id': '001', 'nome': 'Romulo Jackson', 'status': 'Ativo'},
+        {'id': '002', 'nome': 'David Quilan', 'status': 'Ativo'},
+        {'id': '003', 'nome': 'Nayan Jonhson', 'status': 'Inativo'},
+        {'id': '004', 'nome': 'Cassio Dylan', 'status': 'Ativo'},
+        {'id': '005', 'nome': 'Welligton Clinton', 'status': 'Inativo'},
+        {'id': '006', 'nome': 'Marcos Jordan', 'status': 'Pendente'},
+        {'id': '007', 'nome': 'Lucio Smith', 'status': 'Ativo'},
+        {'id': '008', 'nome': 'Robert Brown ', 'status': 'Ativo'},
+        {'id': '009', 'nome': 'Mary Jackson', 'status': 'Inativo'},
+        {'id': '010', 'nome': 'Enzo Smith', 'status': 'Pendente'},
+        {'id': '011', 'nome': 'Wesley Cleriton', 'status': 'Ativo'},
+        {'id': '012', 'nome': 'Dennis Jonhson', 'status': 'Inativo'},
     ]
     # O django buscara automaticamente dentro da pasta templates
     # 2. Prepara o dicionário de contexto. 
